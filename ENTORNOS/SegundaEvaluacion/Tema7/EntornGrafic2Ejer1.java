243308b538cc73d5e17560f216c7bda7a12b837f
DamJuan <148950672+DamJuan@users.noreply.github.com>
2024-01-31 09:29:12 +0100

    zzzzzzzzz

--- ENTORNOS/SegundaEvaluacion/Tema7/EntornGrafic2Ejer1.java
+++ ENTORNOS/SegundaEvaluacion/Tema7/EntornGrafic2Ejer1.java
@@ -8,13 +8,11 @@ public class EntornGrafic2Ejer1 extends JFrame {
     private JPasswordField passwordField;
 
     public EntornGrafic2Ejer1() {
-        // Configuración del frame
         setTitle("Login Form");
         setSize(300, 150);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        // Creación de los componentes
         JLabel titleLabel = new JLabel("Welcome", SwingConstants.CENTER);
         titleLabel.setFont(new Font("Source Code Pro", Font.BOLD, 24));
 
@@ -28,7 +26,6 @@ public class EntornGrafic2Ejer1 extends JFrame {
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Aquí puedes comprobar las credenciales
                 String username = usernameField.getText();
                 char[] password = passwordField.getPassword();
                 if (checkCredentials(username, new String(password))) {
@@ -37,31 +34,25 @@ public class EntornGrafic2Ejer1 extends JFrame {
                     showMessageDialog("Login Failed", "Incorrect username or password.");
                 }
 
-                // Borra la contraseña por seguridad
                 passwordField.setText("");
             }
         });
 
-        // Configuración del layout
         setLayout(new GridLayout(4, 2));
 
-        // Añadir componentes al frame
         add(titleLabel);
-        add(new JLabel()); // Espacio en blanco
+        add(new JLabel());
         add(usernameLabel);
         add(usernameField);
         add(passwordLabel);
         add(passwordField);
-        add(new JLabel()); // Espacio en blanco
+        add(new JLabel());
         add(loginButton);
 
-        // Mostrar el frame
         setVisible(true);
     }
 
     private boolean checkCredentials(String username, String password) {
-        // Aquí puedes implementar la lógica para comprobar las credenciales con tu lógica de backend
-        // En este ejemplo, solo comprobamos que el nombre de usuario sea "admin" y la contraseña "admin123"
         return username.equals("admin") && password.equals("admin123");
     }
 
---
+++ ENTORNOS/SegundaEvaluacion/Tema7/out/production/Tema7/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+G
+EntornGrafic2Ejer1.java,6\b\6bb4b94458fe792087b244ec699860e9faee7b9c
+G
+EntornGrafic2Ejer2.java,3\b\3bf5f36b0f62165b49234b93816e6448f24bd73a
