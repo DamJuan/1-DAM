1e82d2f19f32055ab93a09a8716592ef7d54f79e
DamJuan <148950672+DamJuan@users.noreply.github.com>
2024-03-26 11:22:12 +0100

    asd

--- PROGRAMACION/TerceraEvaluacion/SistemaEncriptacion/src/Main.java
+++ PROGRAMACION/TerceraEvaluacion/SistemaEncriptacion/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Usuario {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Desea encriptar o desencriptar algun archivo: ");
