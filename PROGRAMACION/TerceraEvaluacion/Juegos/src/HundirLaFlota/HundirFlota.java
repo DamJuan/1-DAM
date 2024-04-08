81c38080b407d2fbaa0cdefff572242cc038e7e4
DamJuan <jualopfan@alu.edu.gva.es>
2024-04-08 22:25:27 +0200

    hundir

---
+++ PROGRAMACION/TerceraEvaluacion/Juegos/src/HundirLaFlota/HundirFlota.java
@@ -0,0 +1,21 @@
+package HundirLaFlota;
+
+public class HundirFlota {
+    public static void main(String[] args) {
+        // Crear un objeto de la clase Tablero
+        Tablero tablero = new Tablero();
+        // Llamar al método jugar
+        tablero.jugar();
+    }
+
+    //TODO 1: Mostrar las instrucciones del juego si no se conocen
+    //TODO 2: Preguntar si se quiere jugar contra jugador o contra la máquina
+    //TODO 3: MIRAR COMO HACER (Si se juega contra la máquina, preguntar si se quiere jugar en modo fácil o difícil)
+    //TODO 4: Preguntar nombre del jugador o jugadores
+    //TODO 5: Mostrar el tablero del jugador 1 y los barcos disponibles
+    //TODO 6: Preguntar al jugador 1 dónde quiere colocar los barcos
+    //TODO 7: Mostrar el tablero del jugador 2 y los barcos disponibles
+    //TODO 8: Preguntar al jugador 2 dónde quiere colocar los barcos
+    //TODO 9: Comenzar el juego
+
+}
