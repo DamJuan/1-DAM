cea01388559ce0b40e476615d8eda99282fd5cb5
DamJuan <Jualopfan@alu.edu.gva.es>
2024-06-03 15:23:20 +0200

    a

--- BASE_DE_DATOS/TerceraEvaluacion/Tema8/Soluciones/09.sql
+++
@@ -1,8 +0,0 @@
-SELECT    E.DORSAL, NOMBRE, COUNT(*)  
-FROM      ETAPA E, CICLISTA C 
-WHERE     E.DORSAL=C.DORSAL AND
-          NOMEQ IN (SELECT    NOMEQ
-                    FROM      CICLISTA
-                    GROUP BY  NOMEQ
-                    HAVING    COUNT(*)>5)
-GROUP BY  E.DORSAL, NOMBRE;
